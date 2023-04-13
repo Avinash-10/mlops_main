d939307e2fd50eca766d5330fb1df5b7ce0c008c
Avinash Vyas <avinash1999.vyas@gmail.com>
2023-04-13 11:37:51 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It's a wafer replacement project using MLOPS',
+    author='Avinash Vyas',
+    license='MIT',
+)
